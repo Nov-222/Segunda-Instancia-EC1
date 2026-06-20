95a2ea1317b913c86ab14a06fa4953490c211cc5
Nov-222 <is.gt.lar@gmail.com>
2026-06-20 09:35:12 -0400

    Subiendo configuracion para emular TDD en HU2

--- Backend/Servicios/ReservarServicio.cs
+++ Backend/Servicios/ReservarServicio.cs
@@ -24,31 +24,7 @@ namespace Backend.Servicios
 
         public bool Confirmar_Reserva(ReservarEstadiaDTO Datos)
         {
-            var HabitacionesDisponibles = Repositorio.Obtener_Habitaciones(Datos.Fecha_Inicio, Datos.Fecha_Finalizacion);
-
-            var HabitacionLibre = HabitacionesDisponibles.Any(h => h.Id == Datos.Id_Habitacion);
-
-            if (!HabitacionLibre) return false;
-
-            var InfoHabitacion = HabitacionesDisponibles.First(h => h.Id == Datos.Id_Habitacion);
-
-            int DiasEstadia = (Datos.Fecha_Finalizacion.Date - Datos.Fecha_Inicio.Date).Days;
-
-
-            int PrecioTotalCalculado = Calcular_Costo(DiasEstadia, InfoHabitacion.Precio_Noche);
-
-            int IdNuevaEstadia = Repositorio.Guardar_Estadia(Datos, PrecioTotalCalculado);
-
-            if (IdNuevaEstadia > 0)
-            {
-                foreach (string Documento in Datos.Documentos_Huespedes)
-                {
-                    Repositorio.Registrar_Estadia(IdNuevaEstadia, Documento);
-                }
-                return true;
-            }
-
-            return false;
+            throw new NotImplementedException();
         }
 
         public int Calcular_Costo(int Dias_Estadia, int Precio_Por_Noche)
