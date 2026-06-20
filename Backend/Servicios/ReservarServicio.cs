719cf7f82f695bbdf1d0cb651c74ab24ac559fa3
Nov-222 <is.gt.lar@gmail.com>
2026-06-20 09:39:05 -0400

    [HU-02] implementar [Guardar_Estadia] para pasar test

--- Backend/Servicios/ReservarServicio.cs
+++ Backend/Servicios/ReservarServicio.cs
@@ -24,7 +24,31 @@ namespace Backend.Servicios
 
         public bool Confirmar_Reserva(ReservarEstadiaDTO Datos)
         {
-            throw new NotImplementedException();
+            var HabitacionesDisponibles = Repositorio.Obtener_Habitaciones(Datos.Fecha_Inicio, Datos.Fecha_Finalizacion);
+
+            var HabitacionLibre = HabitacionesDisponibles.Any(h => h.Id == Datos.Id_Habitacion);
+
+            if (!HabitacionLibre) return false;
+
+            var InfoHabitacion = HabitacionesDisponibles.First(h => h.Id == Datos.Id_Habitacion);
+
+            int DiasEstadia = (Datos.Fecha_Finalizacion.Date - Datos.Fecha_Inicio.Date).Days;
+
+
+            int PrecioTotalCalculado = Calcular_Costo(DiasEstadia, InfoHabitacion.Precio_Noche);
+
+            int IdNuevaEstadia = Repositorio.Guardar_Estadia(Datos, PrecioTotalCalculado);
+
+            if (IdNuevaEstadia > 0)
+            {
+                foreach (string Documento in Datos.Documentos_Huespedes)
+                {
+                    Repositorio.Registrar_Estadia(IdNuevaEstadia, Documento);
+                }
+                return true;
+            }
+
+            return false;
         }
 
         public int Calcular_Costo(int Dias_Estadia, int Precio_Por_Noche)
