197ce3ce539d53bada50585084f865df19a9ab53
Nov-222 <is.gt.lar@gmail.com>
2026-06-20 13:05:10 -0400

    Configurando Rutas

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend",
-        policy => policy.WithOrigins("http://127.0.0.1:5500", "http://localhost:5500") // El puerto de Live Server
+        policy => policy.WithOrigins("http://127.0.0.1:5500", "http://localhost:5500", "https://segunda-instancia-ec1.onrender.com") // El puerto de Live Server
                         .AllowAnyMethod()
                         .AllowAnyHeader());
 });
--- Frontend/Services/service.js
+++ Frontend/Services/service.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5192/api";
+const BASE_URL = "https://segunda-instancia-ec1-production.up.railway.app/api";
 
 export const service = {
     async obtenerDisponibilidad(inicio, fin) {
